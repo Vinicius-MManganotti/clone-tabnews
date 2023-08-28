9480afc51a3c612ac57daa07a1f5aa3f5cc45a19
Vinicius Mantovani Manganotti <97189917+Vinicius-MManganotti@users.noreply.github.com>
2023-08-28 12:24:36 +0000

    dia4

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Teste 1</h1>
+}
+
+// O comando export renderiza o compoente par fora do arquivo, na página. O default define ele como padrão para todas as páginas
+export default Home;
