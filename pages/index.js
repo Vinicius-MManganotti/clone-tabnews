0ca7aa8f59df3170b786b7562afa0eff280d3175
Vinicius Mantovani Manganotti <97189917+Vinicius-MManganotti@users.noreply.github.com>
2023-09-21 21:05:47 +0000

    adiciona scripts lint

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá Raquel, japacasa</h1>
+  return <h1>Olá Raquel, japacasa</h1>;
 }
 
 // O comando export renderiza o compoente par fora do arquivo, na página. O default define ele como padrão para todas as páginas
