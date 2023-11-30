cd3263ba9341b37aa2b3a72d5a941424727eefcc
Vinicius Mantovani Manganotti <97189917+Vinicius-MManganotti@users.noreply.github.com>
2023-11-30 21:18:40 +0000

    add tests folder

---
+++ models/calculadora.js
@@ -0,0 +1,8 @@
+exports.somar = somar;
+
+function somar(numero1, numero2) {
+  if (typeof numero1 !== "number") {
+    return "Erro";
+  }
+  return numero1 + numero2;
+}
--- tests/calculadora.test.js
+++
@@ -1,4 +0,0 @@
-// teste com uma arrow function
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
-});
