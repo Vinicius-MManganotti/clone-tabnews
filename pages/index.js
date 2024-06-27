1cf8b9bdb7af1eb81e208f4a949073f181d01c35
Vinicius Mantovani Manganotti <97189917+Vinicius-MManganotti@users.noreply.github.com>
2024-06-27 21:00:38 +0000

    add-script

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,10 @@ function Home() {
   return <h1>Home</h1>;
 }
 
+<script
+  type="text/javascript"
+  async
+  src="https://d2umgtt5tsqx05.cloudfront.net/js/loader-scripts/97429ccf-c186-4c02-bf90-c0644a6c4ea2-loader.js"
+></script>;
 // O comando export renderiza o compoente par fora do arquivo, na página. O default define ele como padrão para todas as páginas
 export default Home;
